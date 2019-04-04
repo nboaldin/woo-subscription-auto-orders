9b3120a5c04ffa4f727493fc2368a4fb2ae5800f
Nathan Boaldin <nboaldin@gmail.com>
2019-04-04 00:09:44 -0500

    cron jobs and such

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 require('dotenv').config()
-
-
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,7 +7,8 @@ const Schema = mongoose.Schema;
 const bodyParser = require('body-parser');
 const WooCommerceAPI = require('woocommerce-api');
 const axios = require('axios');
-const order = require('./testData.json');
+// const order = require('./testData.json');
+const cron = require('node-cron');
  
 const WooCommerce = new WooCommerceAPI({
     url: 'https://kingdomsgf.com',
@@ -24,6 +23,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 
 const orderSchema = new Schema({
     date: { type : Date, default: Date.now },
+    order_forced: Boolean,
     payment_method: String,
     payment_method_title: String,
     set_paid: Boolean,
@@ -59,119 +59,10 @@ const orderSchema = new Schema({
 
 const Order = mongoose.model("Order", orderSchema);
 
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 const baseUrl = 'https://kingdomsgf.com/wp-json/wc/v3';
-
-    if(order.status === "processing") {
-
-        const items = order.line_items;
-        let verify = false;
-
-        for(i = 0; i < items.length; i++) {
-            if(items[i].name.includes('2 - 12oz Bags Monthly') ||  items[i].name.includes('4 - 12oz Bags Monthly')) {
-                verify = true;
-            }
-        }
-
-        // console.log(verify); 
-        
-        if(verify) {
-
-            const newOrder = new Order({
-                payment_method: order.billing.payment_method,
-                payment_method_title: order.billing.payment_method_title,
-                set_paid: false,
-                status: "processing",
-                total: "0.00",
-                order_type: "renewal_order", 
-                parent_id: order.parent_id,
-                billing: {
-                  first_name: order.billing.first_name,
-                  last_name: order.billing.last_name,
-                  address_1: order.billing.address_1,
-                  address_2: order.billing.address_2,
-                  city: order.billing.city,
-                  state: order.billing.state,
-                  postcode: order.billing.postcode,
-                  country: order.billing.country,
-                  email: order.billing.email,
-                  phone: order.billing.phone
-                },
-                shipping: {
-                  first_name: order.shipping.first_name,
-                  last_name: order.shipping.last_name,
-                  address_1: order.shipping.address_1,
-                  address_2: order.shipping.address_2,
-                  city: order.shipping.city,
-                  state: order.shipping.state,
-                  postcode: order.shipping.postcode,
-                  country: order.shipping.country
-                },
-                line_items: order.line_items,
-                shipping_lines: order.shipping_lines
-            });
-            
-            // newOrder.save(function (err) {
-            //     if (err) {
-            //         console.log(err);
-            //     } else {
-            //         console.log('Saved to db');
-            //     }
-            //   });
-
-            Order.find({},'-_id line_items shipping_lines' , function(err, orders) {
-                if(err) {
-                    console.log(err);
-                }
-                for(i = 0; i < orders.length; i++) {
-                    // Taken from Punit Jajodia https://www.toptal.com/software/definitive-guide-to-datetime-manipulation
-                    const dateOfOrder = orders[i].date;
-                    const now = new Date();
-                    const datefromAPITimeStamp = (new Date(dateOfOrder)).getTime();
-                    const nowTimeStamp = now.getTime();
-
-                    const microSecondsDiff = Math.abs(datefromAPITimeStamp - nowTimeStamp );
-                    // Number of milliseconds per day =
-                    //   24 hrs/day * 60 minutes/hour * 60 seconds/minute * 1000 msecs/second
-                    const daysDiff = Math.floor(microSecondsDiff/(1000 * 60 * 60  * 24));
-
-
-                    if (daysDiff === 15) {
-
-                        // ?consumer_key=${process.env.CONS_KEY}&consumer_secret=${process.env.CONS_SEC}
-
-                        // console.log(orders[i]);
-
-                        axios.post(baseUrl + `/orders?consumer_key=${process.env.CONS_KEY}&consumer_secret=${process.env.CONS_SEC}`, orders[i])
-                        .then(function (response) {
-                            console.log(response.data);
-
-                        })
-                        .catch(function (error) {
-                            // handle error
-                            console.log(error.response.status);
-                            // console.log(error);
-                        })
-                        .then(function () {
-                            
-                        });
-                    }
-                }
-            });
-
-        }
-
-
-        
-    }
-
-
 
 
 app.get('/', (req, res, next) => { 
@@ -183,7 +74,148 @@ app.get('/', (req, res, next) => {
 app.post('/', (req, res, next) => { 
     res.sendStatus(200);
     res.end();
+
+    const order = req.body;
+
+    //If incoming order was just created by this program, disregard it as to not create an infinite loop.
+    if(order.created_via === "rest-api") {
+        console.log('Order was already created with rest-api. Will not place new order.');
+        return
+    } else {
+        // If order has been paid for
+        if(order.status === "processing") {
+
+            const items = order.line_items;
+            let verify = false;
+
+            // Check all line items to see if they are within a subscription that needs to ship more than once a month
+            for(i = 0; i < items.length; i++) {
+                if(items[i].name.includes('2 - 12oz Bags Monthly') ||  items[i].name.includes('4 - 12oz Bags Monthly')) {
+                    verify = true;
+                }
+            }
+            
+            // If items need to ship more than once a month
+            if(verify) {
+    
+            // Create new order based on order just recieved.     
+            const newOrder = new Order({
+                order_forced: true,
+                payment_method: order.billing.payment_method,
+                payment_method_title: order.billing.payment_method_title,
+                set_paid: false,
+                status: "processing",
+                total: "0.00",
+                order_type: "renewal_order", 
+                parent_id: order.parent_id,
+                billing: {
+                    first_name: order.billing.first_name,
+                    last_name: order.billing.last_name,
+                    address_1: order.billing.address_1,
+                    address_2: order.billing.address_2,
+                    city: order.billing.city,
+                    state: order.billing.state,
+                    postcode: order.billing.postcode,
+                    country: order.billing.country,
+                    email: order.billing.email,
+                    phone: order.billing.phone
+                },
+                shipping: {
+                    first_name: order.shipping.first_name,
+                    last_name: order.shipping.last_name,
+                    address_1: order.shipping.address_1,
+                    address_2: order.shipping.address_2,
+                    city: order.shipping.city,
+                    state: order.shipping.state,
+                    postcode: order.shipping.postcode,
+                    country: order.shipping.country
+                },
+                line_items: order.line_items,
+                shipping_lines: order.shipping_lines
+            });
+                
+            // Save the new order to db
+            newOrder.save(function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Saved to db');
+                }
+                });
+
+            }   
+        }
+    }
     
 });
+
+//Run a cron job every day that queries all orders stored in db
+const queryschedule = cron.schedule('0,10 * * * * *', () => {
+    console.log('Runing a job at 08:00 at America/Sao_Paulo timezone');
+
+    Order.find({},'-_id -line_items -shipping_lines', function(err, orders) {
+        if(err) {
+            console.log(err);
+        }
+        for(i = 0; i < orders.length; i++) {
+            // Taken from Punit Jajodia https://www.toptal.com/software/definitive-guide-to-datetime-manipulation
+            const dateOfOrder = orders[i].date;
+            const now = new Date();
+            const datefromAPITimeStamp = (new Date(dateOfOrder)).getTime();
+            const nowTimeStamp = now.getTime();
+
+            const microSecondsDiff = Math.abs(datefromAPITimeStamp - nowTimeStamp );
+            // Number of milliseconds per day =
+            //   24 hrs/day * 60 minutes/hour * 60 seconds/minute * 1000 msecs/second
+            const daysDiff = Math.floor(microSecondsDiff/(1000 * 60 * 60  * 24));
+            
+            console.log(daysDiff);
+
+            // If it is exactly 15 days from the triggering order, post the new order
+            if (daysDiff === 15) {
+
+                // ?consumer_key=${process.env.CONS_KEY}&consumer_secret=${process.env.CONS_SEC}
+
+                //console.log(orders[i]);
+
+                // axios.post(baseUrl + `/orders?consumer_key=${process.env.CONS_KEY}&consumer_secret=${process.env.CONS_SEC}`, orders[i])
+                // .then(function (response) {
+                //     console.log(response.data);
+
+                // })
+                // .catch(function (error) {
+                //     // handle error
+                //     console.log(error.response.status);
+                //     // console.log(error);
+                // })
+                // .then(function () {
+                    
+                // });
+            }
+
+            if (daysDiff === 0) {
+                //Remove orders that are 25 days old from the db
+                orders[i].remove(function(err,result) {
+                    if(err) {
+                        console.log(err);
+                    }
+                    console.log(result);
+                });
+            }
+        }
+    });
+
+}, {
+    scheduled: true,
+    timezone: "America/Sao_Paulo"
+  });
+
+// axios.get(baseUrl + `/orders/73112?consumer_key=${process.env.CONS_KEY}&consumer_secret=${process.env.CONS_SEC}`)
+//     .then(function(response) {
+//         console.log(response.data.created_via)
+//     })
+//     .catch(function(err) {
+//         console.log(err)
+//     });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
